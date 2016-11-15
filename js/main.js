06ccc846a770c6158897cf123f89a399f212ad5e
VikingRock <a.zinchuk.a@gmail.com>
2016-11-16 00:10:22 +0300

    fixed js es2015 syntax + fixed js error

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
 (function () {
 
-  var template = document.querySelector('template');
+  let template = document.querySelector('template');
 
-  var loadTemplate = function (templateName) {
-    var content = template.content ? template.content : template;
+  let loadTemplate = (templateName) => {
+    let content = template.content ? template.content : template;
     return content.querySelector(templateName).cloneNode(true);
   };
 
-
-  var slides = [
+  let slides = [
     loadTemplate('.main--welcome'),
     loadTemplate('.main--level-artist'),
     loadTemplate('.main--level-genre'),
     loadTemplate('.main--result')
   ];
-  var current = -1;
+  let slidesLength = slides.length;
+  let current = 0;
 
-  var select = function (index) {
-    current = index;
-    var mainElement = document.querySelector('.main');
+  let select = (index) => {
+    let mainElement = document.querySelector('.main');
     mainElement.parentNode.replaceChild(slides[index], mainElement);
   };
 
-  document.onkeydown = function (evt) {
+  document.onkeydown = (evt) => {
     evt.preventDefault();
 
     switch (evt.keyCode) {
       case 37: current--; break;
       case 39: current++; break;
+    }
+
+    if (current < 0) {
+      current++;
+      return;
+    } else if (current >= slidesLength) {
+      current--;
+      return;
     }
 
     select(current);
