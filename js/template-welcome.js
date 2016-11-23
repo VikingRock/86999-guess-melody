d33eeaae179d6517f21b0d8cc1a8342713cfaa98
VikingRock <a.zinchuk.a@gmail.com>
2016-11-23 22:31:58 +0300

    welcome module refactored

--- js/template-welcome.js
+++ js/template-welcome.js
@@ -2,20 +2,40 @@ import * as dom from 'dom-helpers';
 import artistElement from 'template-artist';
 
 /**
+ * welcome page data structure
+ * @const
+ * @type {object}
+ */
+const welcome = {
+  game_name: 'Угадай мелодию',
+  play_button: 'Начать игру',
+  content: {
+    title: 'Правила игры',
+    text: 'Правила просты&nbsp;— за&nbsp;2 минуты дать максимальное количество правильных ответов. ' +
+          'На&nbsp;каждую мелодию всего 3 варианта ответа. Удачи!'
+  }
+};
+
+const logo = `<section class="logo" title="${welcome.game_name}"><h1>${welcome.game_name}</h1></section>`;
+
+const button = `<button class="main-play">${welcome.play_button}</button>`;
+
+const content = `
+  <h2 class="title main-title">${welcome.content.title}</h2>
+  <p class="text main-text">
+    ${welcome.content.text}
+  </p>`;
+
+
+/**
  * welcome page template
  * @const
  * @type {string}
  */
 const moduleString = `<section class="main main--welcome">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-    <button class="main-play">Начать игру</button>
-    <h2 class="title main-title">Правила игры</h2>
-    <p class="text main-text">
-      Правила просты&nbsp;— за&nbsp;2 минуты дать
-      максимальное количество правильных ответов.<br>
-      На&nbsp;каждую мелодию всего 3 варианта ответа.<br>
-      Удачи!
-    </p>
+    ${logo}
+    ${button}
+    ${content}
   </section>`;
 
 /**
